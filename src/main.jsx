7a7e53e9202a0aa342d5663f0423da59e24b884a
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-12 14:03:29 +0300

    Add router configuration for main application route

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { store } from "./store";
-import { Provider } from "react-redux";
+import { Provider as StoreProvider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <StoreProvider store={store}>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </StrictMode>
 );
