23b455b84fa86f53b64f5740c89eb33a7407d2e2
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-13 15:08:17 +0300

    add querry client and provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import { store } from "./store";
 import { Provider as StoreProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StoreProvider store={store}>
-      <RouterProvider router={router} />
-    </StoreProvider>
+    <QueryClientProvider client={queryClient}>
+      <StoreProvider store={store}>
+        <RouterProvider router={router} />
+      </StoreProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
