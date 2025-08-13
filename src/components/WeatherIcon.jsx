ed6e9c99ad4029ff04d4378960736ddc4215dd21
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-13 15:09:32 +0300

    create weather icon component

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiCloudy,
+  WiShowers,
+  WiDayRain,
+  WiNightAltRain,
+  WiThunderstorm,
+  WiSnow,
+  WiFog,
+  WiNa,
+} from "react-icons/wi";
+
+const WeatherIcon = ({ iconCode, ...props }) => {
+  const iconMap = {
+    "01d": <WiDaySunny {...props} />,
+    "01n": <WiNightClear {...props} />,
+    "02d": <WiDayCloudy {...props} />,
+    "02n": <WiNightAltCloudy {...props} />,
+    "03d": <WiCloud {...props} />,
+    "03n": <WiCloud {...props} />,
+    "04d": <WiCloudy {...props} />,
+    "04n": <WiCloudy {...props} />,
+    "09d": <WiShowers {...props} />,
+    "09n": <WiShowers {...props} />,
+    "10d": <WiDayRain {...props} />,
+    "10n": <WiNightAltRain {...props} />,
+    "11d": <WiThunderstorm {...props} />,
+    "11n": <WiThunderstorm {...props} />,
+    "13d": <WiSnow {...props} />,
+    "13n": <WiSnow {...props} />,
+    "50d": <WiFog {...props} />,
+    "50n": <WiFog {...props} />,
+  };
+
+  return iconMap[iconCode] || <WiNa {...props} />;
+};
+
+export default WeatherIcon;
