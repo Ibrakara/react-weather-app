0f3b96578358067ab8b452e6e14bdce5078055a4
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-13 15:09:04 +0300

    change services paths and remove unnecesarry one

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -18,37 +18,24 @@ export const getCurrentWeather = async (city, units = "metric") => {
     throw error;
   }
 };
-export const getWeeklyForecast = async (lat, lon, units = "metric") => {
+export const getWeeklyForecast = async (lat, lon) => {
   try {
-    const response = await axios.get(`${BASE_URL}/onecall`, {
+    const response = await axios.get(`${BASE_URL}/forecast`, {
       params: {
         lat,
         lon,
         exclude: "hourly,minutely",
-        units,
         appid: API_KEY,
       },
     });
+    console.log(
+      "Weekly forecast data:",
+      new Date(response.data.list[0].dt),
+      response.data.list[0].dt
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching weekly forecast:", error);
-    throw error;
-  }
-};
-
-export const getWeatherByCoords = async (lat, lon, units = "metric") => {
-  try {
-    const response = await axios.get(`${BASE_URL}/weather`, {
-      params: {
-        lat,
-        lon,
-        units,
-        appid: API_KEY,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching weather by coordinates:", error);
     throw error;
   }
 };
