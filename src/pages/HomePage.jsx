365b0d8e239b63ae3f76aab38c82336554579d7f
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-14 11:56:02 +0300

    add weathercard and weather data queries

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
+import { useQuery } from "@tanstack/react-query";
+import {
+  getCurrentWeather,
+  getThreeHourlyForecast,
+} from "../services/weatherService.js";
+import WeatherCard from "../components/WeatherCard.jsx";
+import { getDailyForecast } from "../services/helpers.js";
 
 function HomePage() {
-  const handleSearch = (query) => {
-    console.log("Searching for:", query);
-    // Add your search logic here
+  const [locationName, setLocationName] = useState("");
+  const handleSearch = (location) => {
+    setLocationName(location);
   };
+  const {
+    isLoading: cityIsLoading,
+    data: cityData,
+    error: cityError,
+    isSuccess: cityIsSuccess,
+  } = useQuery({
+    queryKey: ["cityWeather", locationName],
+    queryFn: () => getCurrentWeather(locationName),
+    staleTime: 1000 * 60 * 5,
+    enabled: !!locationName,
+    retry: false,
+  });
+  const {
+    isLoading: forecastIsLoading,
+    data: forecastData,
+    error: forecastError,
+  } = useQuery({
+    queryKey: [
+      "weeklyForecast",
+      cityData?.coord?.lat,
+      cityData?.coord?.lon,
+      locationName,
+    ],
+    queryFn: () =>
+      getThreeHourlyForecast(cityData?.coord?.lat, cityData?.coord?.lon),
+    staleTime: 1000 * 60 * 5,
+    enabled: !!cityData?.coord?.lat && !!cityData?.coord?.lon && cityIsSuccess,
+    retry: false,
+    select: (data) => getDailyForecast(data),
+  });
   return (
     <>
       <SearchBar onSearch={handleSearch} />
+      {cityIsLoading && <p>Loading...</p>}
+      {cityError && <p>Error: {cityError.message}</p>}
+      {cityData && <WeatherCard weatherData={cityData} />}
+      {!cityIsLoading && !cityError && !cityData && (
+        <p>No weather data available.</p>
+      )}
     </>
   );
 }
