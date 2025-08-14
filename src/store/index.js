57c2a3ad9af61a8ce2f52ca9d7e929be153c134c
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-14 15:06:59 +0300

    save theme state to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import locationReducer from "./slices/locationSlice";
 import languagleReducer from "./slices/languageSlice";
+import { loadState, saveState } from "../services/helpers";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +12,11 @@ export const store = configureStore({
     location: locationReducer,
     language: languagleReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  saveState({
+    theme: store.getState().theme,
+  });
 });
