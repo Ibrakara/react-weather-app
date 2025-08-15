01faad17e119770e1a199f12401a8d061e0486ff
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-15 15:24:01 +0300

    create forecastCard component

---
+++ src/components/ForecastCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import styles from "../styles/ForecastCard.module.css";
+
+function ForecastCard({ dateString, iconCode, temp, description }) {
+  return (
+    <div className={styles.card}>
+      <p className={styles.dateTime}>{dateString}</p>
+      <WeatherIcon iconCode={iconCode} size={40} className={styles.icon} />
+      <div className={styles.temps}>
+        <span className={styles.temp}>{temp}°C</span>
+      </div>
+      {description && <p className={styles.description}>{description}</p>}
+    </div>
+  );
+}
+
+export default ForecastCard;
