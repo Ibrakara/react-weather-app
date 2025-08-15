23efd37c2fc10319cdcadc3e91b8d10145852f29
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-15 14:27:17 +0300

    add data to the screen

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import HourlyForecast from "../components/HourlyForecast";
+import ErrorMessage from "../components/ErrorMessage";
+import { getErrorMessage } from "../services/helpers";
+import { useWeeklyForecast } from "../hooks/useForecastList";
 
-function DetailPage() {
+const DetailPage = () => {
+  const { locationDate } = useParams();
+
+  const date = locationDate.split("-").slice(1).join("-");
+  const locationName = locationDate.split("-")[0];
+  const {
+    data: forecastData,
+    isLoading,
+    error,
+  } = useWeeklyForecast(null, null, locationName, false);
+
+  const detailedDayData = useMemo(() => {
+    if (!forecastData) return [];
+    return forecastData.list.filter((item) => {
+      const itemDate = new Date((item.dt + forecastData.city.timezone) * 1000)
+        .toISOString()
+        .slice(0, 10);
+      return itemDate === date;
+    });
+  }, [forecastData, date]);
+
+  if (isLoading) {
+    return <p>Loading details...</p>;
+  }
+
+  if (error) {
+    const errorMessage = getErrorMessage(error, null);
+    return <ErrorMessage message={errorMessage} />;
+  }
+
   return (
-    <>
-      <h1>Detail</h1>
-    </>
+    <HourlyForecast
+      data={detailedDayData}
+      locationName={locationName}
+      date={date}
+      timezoneOffset={forecastData.city.timezone}
+    />
   );
-}
+};
 
 export default DetailPage;
