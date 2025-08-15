5d5d125715cd8a57e55fb62d13d5a90db03a6af7
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-15 11:44:35 +0300

    add lat lon values to querry and add location wheater

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../containers/SearchBar";
 import { useQuery } from "@tanstack/react-query";
-import {
-  getCurrentWeather,
-  getThreeHourlyForecast,
-} from "../services/weatherService.js";
+import { getCurrentWeather } from "../services/weatherService.js";
 import WeatherCard from "../components/WeatherCard.jsx";
-import { getDailyForecast } from "../services/helpers.js";
+import { useWeeklyForecast } from "../hooks/useForecastList.js";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCurrentWeather,
+  setForecastList,
+} from "../store/slices/wheaterSlice.js";
 
 function HomePage() {
+  const dispatch = useDispatch();
+  const userLatitude = useSelector((state) => {
+    console.log(state.location.currentLocation?.latitude);
+    return state.location.currentLocation?.latitude;
+  });
+  const userLongitude = useSelector(
+    (state) => state.location.currentLocation?.longitude
+  );
   const [locationName, setLocationName] = useState("");
   const handleSearch = (location) => {
     setLocationName(location);
@@ -19,31 +29,35 @@ function HomePage() {
     error: cityError,
     isSuccess: cityIsSuccess,
   } = useQuery({
-    queryKey: ["cityWeather", locationName],
-    queryFn: () => getCurrentWeather(locationName),
+    queryKey: ["cityWeather", locationName, userLatitude, userLongitude],
+    queryFn: () =>
+      locationName
+        ? getCurrentWeather(locationName)
+        : getCurrentWeather(null, userLatitude, userLongitude),
     staleTime: 1000 * 60 * 5,
-    enabled: !!locationName,
+    enabled: !!locationName || !!(userLatitude && userLongitude),
     retry: false,
   });
   const {
     isLoading: forecastIsLoading,
     data: forecastData,
     isSuccess: forecastIsSuccess,
-    error: forecastError,
-  } = useQuery({
-    queryKey: [
-      "weeklyForecast",
-      cityData?.coord?.lat,
-      cityData?.coord?.lon,
-      locationName,
-    ],
-    queryFn: () =>
-      getThreeHourlyForecast(cityData?.coord?.lat, cityData?.coord?.lon),
-    staleTime: 1000 * 60 * 5,
-    enabled: !!cityData?.coord?.lat && !!cityData?.coord?.lon && cityIsSuccess,
-    retry: false,
-    select: (data) => getDailyForecast(data),
-  });
+  } = useWeeklyForecast(
+    cityData?.coord?.lat,
+    cityData?.coord?.lon,
+    locationName
+  );
+  useEffect(() => {
+    if (cityIsSuccess && cityData) {
+      dispatch(setCurrentWeather(cityData));
+    }
+  }, [cityIsSuccess, cityData, dispatch]);
+  useEffect(() => {
+    if (forecastIsSuccess && forecastData) {
+      dispatch(setForecastList(forecastData));
+    }
+  }, [forecastIsSuccess, forecastData, dispatch]);
+
   return (
     <>
       <SearchBar onSearch={handleSearch} />
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,14 +3,22 @@ import { BASE_URL } from "../constants/index.js";
 
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-export const getCurrentWeather = async (city, units = "metric") => {
+export const getCurrentWeather = async (city, lat, lon, units = "metric") => {
   try {
+    const params = {
+      units,
+      appid: API_KEY,
+    };
+    if (city) {
+      params.q = city;
+    } else if (lat !== null && lon !== null) {
+      params.lat = lat;
+      params.lon = lon;
+    } else {
+      return null;
+    }
     const response = await axios.get(`${BASE_URL}/weather`, {
-      params: {
-        q: city,
-        units,
-        appid: API_KEY,
-      },
+      params: params,
     });
     return response.data;
   } catch (error) {
