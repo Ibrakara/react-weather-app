ee63154c2e3a619210639e17814bb26f1860507c
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-16 18:29:29 +0300

    add toast to bottom right

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <StoreProvider store={store}>
         <RouterProvider router={router} />
+        <Toaster position="bottom-right" />
       </StoreProvider>
     </QueryClientProvider>
   </StrictMode>
