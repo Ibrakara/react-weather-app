6ea1443b81bbcaa3792a966bfdcfa68875918796
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-16 17:17:40 +0300

    insist the data with localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { loadState, saveState } from "../services/helpers";
 import themeReducer from "./slices/themeSlice";
 import locationReducer from "./slices/locationSlice";
-import languagleReducer from "./slices/languageSlice";
+import languageReducer, {
+  languageListenerMiddleware,
+} from "./slices/languageSlice";
 import weatherSliceReducer from "./slices/weatherSlice";
+import i18n from "../i18n";
 
 const preloadedState = loadState();
 
@@ -11,15 +14,23 @@ export const store = configureStore({
   reducer: {
     theme: themeReducer,
     location: locationReducer,
-    language: languagleReducer,
+    language: languageReducer,
     weather: weatherSliceReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(languageListenerMiddleware.middleware),
   preloadedState,
   devTools: process.env.NODE_ENV !== "production",
 });
 
+if (preloadedState?.language?.language) {
+  i18n.changeLanguage(preloadedState.language.language);
+}
+
 store.subscribe(() => {
   saveState({
     theme: store.getState().theme,
+    language: store.getState().language,
+    location: { ...store.getState().location, currentLocation: null },
   });
 });
