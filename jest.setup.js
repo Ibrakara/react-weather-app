32d3b829f6fbd02a058f5fa497a44b67e7104e64
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-17 18:55:30 +0300

    add global mocks

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,47 @@
 import "@testing-library/jest-dom";
 
-// Suppress react-i18next warning about missing i18n instance
-const originalWarn = console.warn;
-console.warn = (...args) => {
-  if (args[0].includes('react-i18next:: useTranslation: You will need to pass in an i18next instance')) {
-    return;
-  }
-  originalWarn.call(console, ...args);
+global.TextEncoder = require("util").TextEncoder;
+
+jest.mock("i18next", () => ({
+  use: () => ({
+    init: jest.fn().mockReturnThis(),
+  }),
+  init: jest.fn().mockReturnThis(),
+  t: (key) => key,
+}));
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key) => key,
+    i18n: {
+      changeLanguage: jest.fn(),
+    },
+  }),
+  initReactI18next: {},
+}));
+
+jest.mock("./src/services/weatherService", () =>
+  jest.requireActual("./__mocks__/src/services/weatherService")
+);
+
+jest.mock("./src/store/index.js", () => ({
+  store: {
+    dispatch: jest.fn(),
+    getState: jest.fn(),
+    subscribe: jest.fn(),
+    replaceReducer: jest.fn(),
+  },
+}));
+
+const mockGeolocation = {
+  getCurrentPosition: jest.fn((success, error) =>
+    error({ code: 1, message: "User denied Geolocation" })
+  ),
+  watchPosition: jest.fn(),
+  clearWatch: jest.fn(),
 };
+
+Object.defineProperty(global.navigator, "geolocation", {
+  value: mockGeolocation,
+  writable: true,
+});
