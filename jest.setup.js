4d6a3483a9019275dd68685cf58b445377033574
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-17 18:10:46 +0300

    silent the console warn for i18n

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom";
+
+// Suppress react-i18next warning about missing i18n instance
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0].includes('react-i18next:: useTranslation: You will need to pass in an i18next instance')) {
+    return;
+  }
+  originalWarn.call(console, ...args);
+};
