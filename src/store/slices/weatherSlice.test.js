308e703b7a9507e8fbd8465ed903880d8270a229
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-17 19:08:33 +0300

    add test for weather store

---
+++ src/store/slices/weatherSlice.test.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+import weatherReducer, { setCurrentWeather, setForecastList } from './weatherSlice';
+
+describe('weatherSlice', () => {
+  let store;
+
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        weather: weatherReducer,
+      },
+    });
+  });
+
+  it('should return the initial state', () => {
+    expect(store.getState().weather.currentWeather).toBeNull();
+    expect(store.getState().weather.forecastList).toEqual([]);
+  });
+
+  it('should handle setCurrentWeather', () => {
+    const weatherData = { temperature: 25, condition: 'Sunny' };
+    store.dispatch(setCurrentWeather(weatherData));
+    expect(store.getState().weather.currentWeather).toEqual(weatherData);
+  });
+
+  it('should handle setForecastList', () => {
+    const forecastData = [{ day: 'Mon', temp: 20 }, { day: 'Tue', temp: 22 }];
+    store.dispatch(setForecastList(forecastData));
+    expect(store.getState().weather.forecastList).toEqual(forecastData);
+  });
+});
