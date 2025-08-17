39be65316eaef7553dafa3284622907df1f972f4
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-08-17 18:55:14 +0300

    add mock service

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ export default {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/services/weatherService$": "<rootDir>/__mocks__/services/weatherService.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [],
 };
